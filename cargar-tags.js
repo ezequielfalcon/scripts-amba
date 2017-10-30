daf043d0321c50c9163b1f9cbe75ddeb44d5e533
Ezequiel Falcón <falconezequiel@gmail.com>
2017-10-30 10:46:55 -0300

    fix de ceros en tags2

--- cargar-tags.js
+++ cargar-tags.js
@@ -11,10 +11,10 @@ const cn = {
 const db = pgp(process.env.DATABASE_URL || cn);
 
 
-for (let i = 0; i < (800); i++){
-    db.none('insert into tags (cod, uid) VALUES ($1, $2);', [i + 990001, (i + 990001) + '0000000000000000000'])
+for (let i = 0; i < (1600); i++){
+    db.none('insert into tags (cod, uid) VALUES ($1, $2);', [i + 99001, (i + 99001) + '00000000000000000000'])
         .then(() => {
-            console.log('Insertado ' + (i + 990001));
+            console.log('Insertado ' + (i + 99001));
         })
         .catch(err => {
             console.log(err);
